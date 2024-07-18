3b2c67ae10b87451031b61c9d2c3261089bb3010
Stas Klymenko <klymenko.stas@gmail.com>
2024-07-18 13:34:15 +0300

    All sections added

--- core.js
+++ core.js
@@ -1,41 +1,43 @@
 const cards = [
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""},
-    {name: "adsa", img: ""}
+    "ВIЗИТКИ",
+    "НАЛIПКИ",
+    "ЛИСТIВКИ",
+    "БIРКИ",
+    "ПОДАРУНКОВI СЕРТИФIКАТИ",
+    "БЛОКНОТИ",
+    "МАГНIТИ",
+    "КАТАЛОГИ",
+    "ПЛАСТИКОВI КАРТИ",
+    "КОНВЕРТИ",
+    "БЛОКИ ДЛЯ ЗАПИСIВ",
+    "КАЛЕНДАРI",
+    "ВИВIСКИ",
+    "БАНЕРИ",
+    "ПОСТЕРИ",
+    "ТАБЛИЧКИ",
+    "ПЛIВКА",
+    "НАЛIПКИ НА АВТО",
+    "МОБIЛЬНI СТЕНДИ",
+    "КАРТИНИ",
+    "МЕТАЛЕВI ТАБЛИЧКИ",
+    "ЧАШКИ",
+    "ФУТБОЛКИ",
+    "КЕПКИ"
 ]
 
 addCards()
 
 function createCards() {
     let list = ""
+    let iter = 1
     for(const card of cards) {
         list += `
-        <div class="w-[100%] h-[7rem] relative">
-            <img class="w-[100%] h-[7rem] object-cover" src="https://picsum.photos/300/200">
-            <p class="absolute top-[0.5rem] left-[0.5rem] right-[0.5rem] text-center bg-black bg-opacity-70 text-white">${card.name}</p>
+        <div class="w-[100%] h-[8rem] relative">
+            <img class="w-[100%] h-[8rem] object-cover" src="./img/${iter}.jpg">
+            <p class="absolute top-[0.5rem] left-[0.5rem] right-[0.5rem] text-center bg-black bg-opacity-70 text-white">${card}</p>
         </div>
         `
+        iter++
     }
     return list
 }
