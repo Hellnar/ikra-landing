54d9868cd4ec01e009f9b6fb12bf31915ba2fd44
Stas Klymenko <klymenko.stas@gmail.com>
2024-07-21 22:18:50 +0300

    Add popup functionality

--- core.js
+++ core.js
@@ -26,14 +26,14 @@ const cards = [
 ]
 
 addCards()
-popup()
+openPopup()
 
 function createCards() {
     let list = ""
     let iter = 1
     for(const card of cards) {
         list += `
-        <div class="w-[100%] h-[8rem] relative">
+        <div class="card w-[100%] h-[8rem] relative hover:cursor-pointer">
             <img class="w-[100%] h-[8rem] object-cover" src="./img/${iter}.jpg">
             <p class="absolute top-[0.5rem] left-[0.5rem] right-[0.5rem] text-center font-bold bg-black bg-opacity-70 text-white">${card}</p>
         </div>
@@ -47,12 +47,25 @@ function addCards() {
     document.getElementById("cards").innerHTML = createCards()
 }
 
+function openPopup() {
+    document.querySelectorAll(".card").forEach(card => card.addEventListener("click", () => popup()))
+}
+
+function closePopup() {
+    document.getElementById("close-popup").addEventListener("click", () => document.getElementById("popup").remove())
+    document.getElementById("popup").addEventListener("click", (e) => {
+        if(e.target === e.currentTarget) document.getElementById("popup").remove()
+    })
+}
+
 function popup() {
     const window = document.createElement("div")
-    window.className = "w-[100vw] h-[100vh] fixed top-0 left-0 flex justify-center items-center"
+    window.id = "popup"
+    window.className = "w-[100vw] h-[100vh] fixed top-0 left-0 flex justify-center items-center bg-black bg-opacity-80"
     window.innerHTML = `
-    <div class="w-[60%] bg-white rounded-lg flex flex-col items-center p-6">
-        <div class="flex items-center">
+    <div class="md:w-[60%] sm:w-[80%] bg-white rounded-lg flex flex-col gap-4 items-center relative p-6">
+        <button id="close-popup" class="absolute top-2 right-2 font-bold text-xl px-4 py-2 hover:bg-gray-200 rounded-lg">x</button>
+        <div class="flex items-center gap-4">
             <p>Напишiть нам у: </p>
             <div class="flex gap-4 items-center">
                 <img class="w-8 h-8" src="./img/w_insta.png">   
@@ -61,7 +74,7 @@ function popup() {
                 <img class="w-8 h-8" src="./img/w_whatsapp.png">   
             </div>
         </div>
-        <div class="flex items-center gap-4">
+        <div class="flex flex-col items-center gap-4">
             <p>Або залиште свiй номер </p>
             <input type="text" class="border">
             <p>ми Вам зателефонуємо</p>
@@ -69,4 +82,5 @@ function popup() {
     </div>
     `
     document.body.appendChild(window)
+    closePopup()
 }
